5f7ac7c3aae66a95dba9ae6cfb049d7b9826891a
Bunyapon Chaiongkarn <88047496+ElCappucino@users.noreply.github.com>
2024-09-09 15:44:53 +0700

    Publish Engine Structure

---
+++ KrillKrew/KrillEngine/KrillEngine/Level.cpp
@@ -0,0 +1,82 @@
+#include "Level.h"
+
+
+void Level::LevelLoad()
+{
+	SquareMeshVbo * square = new SquareMeshVbo();
+	square->LoadData();
+	GameEngine::GetInstance()->AddMesh(SquareMeshVbo::MESH_NAME, square);
+
+	//cout << "Load Level" << endl;
+}
+
+void Level::LevelInit()
+{
+	ImageObject * obj = new ImageObject();
+	obj->SetTexture("../Resource/Texture/Prinny.png");
+	obj->SetSize(1.5f, -1.5f);
+	objectsList.push_back(obj);
+
+	player = obj;
+
+	//cout << "Init Level" << endl;
+}
+
+void Level::LevelUpdate()
+{
+	//cout << "Update Level" << endl;
+}
+
+void Level::LevelDraw()
+{
+	GameEngine::GetInstance()->Render(objectsList);
+	//cout << "Draw Level" << endl;
+}
+
+void Level::LevelFree()
+{
+	for (DrawableObject* obj : objectsList) {
+		delete obj;
+	}
+	objectsList.clear();
+	//cout << "Free Level" << endl;
+}
+
+void Level::LevelUnload()
+{
+	GameEngine::GetInstance()->ClearMesh();
+	//cout << "Unload Level" << endl;
+}
+
+void Level::HandleKey(char key)
+{
+
+	switch (key)
+	{
+		case 'w': player->Translate(glm::vec3(0, 0.3, 0)); break;
+		case 's': player->Translate(glm::vec3(0, -0.3, 0)); break;
+		case 'a': player->Translate(glm::vec3(-0.3, 0, 0)); break;
+		case 'd': player->Translate(glm::vec3(0.3, 0, 0)); break;
+		case 'q': GameEngine::GetInstance()->GetStateController()->gameStateNext = GameState::GS_QUIT; ; break;
+		case 'r': GameEngine::GetInstance()->GetStateController()->gameStateNext = GameState::GS_RESTART; ; break;
+		case 'e': GameEngine::GetInstance()->GetStateController()->gameStateNext = GameState::GS_LEVEL2; ; break;
+	}
+}
+
+void Level::HandleMouse(int type, int x, int y)
+{
+	float realX, realY;
+
+	// Calculate Real X Y 
+	realX = x;
+	realY = y;
+
+	GameEngine::GetInstance()->GetWindowWidth();
+	GameEngine::GetInstance()->GetWindowHeight();
+
+	cout << "X : " << realX << " Y : " << realY << endl;
+
+	player->SetPosition(glm::vec3(realX, realY, 0));
+}
+
+
