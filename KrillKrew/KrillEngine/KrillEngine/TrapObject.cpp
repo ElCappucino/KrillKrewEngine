942896f3eecf347a207427df6efbcad3fc9e99c9
RodITimMaa <88047496+ElCappucino@users.noreply.github.com>
2024-11-11 15:31:50 +0700

    import trap object class

---
+++ KrillKrew/KrillEngine/KrillEngine/TrapObject.cpp
@@ -0,0 +1,80 @@
+#include "TrapObject.h"
+#include "GameEngine.h"
+#include "SquareMeshVbo.h"
+
+
+TrapObject::TrapObject()
+{
+
+}
+
+
+TrapObject::~TrapObject()
+{
+}
+
+void TrapObject::SetTexture(std::string path)
+{
+	texture = GameEngine::GetInstance()->GetRenderer()->LoadTexture(path);
+}
+
+void TrapObject::SetSheetInfo(float row, float column, float spritewidth, float spriteheight, float sheetwidth, float sheetheight) {
+	sheetInfo.row = row;
+	sheetInfo.column = column;
+	sheetInfo.spritewidth = spritewidth;
+	sheetInfo.spriteheight = spriteheight;
+	sheetInfo.sheetwidth = sheetwidth;
+	sheetInfo.sheetheight = sheetheight;
+}
+
+void TrapObject::Render(glm::mat4 globalModelTransform)
+{
+	SquareMeshVbo* squareMesh = dynamic_cast<SquareMeshVbo*> (GameEngine::GetInstance()->GetRenderer()->GetMesh(SquareMeshVbo::MESH_NAME));
+
+	GLuint modelMatixId = GameEngine::GetInstance()->GetRenderer()->GetModelMatrixAttrId();
+	GLuint renderModeId = GameEngine::GetInstance()->GetRenderer()->GetModeUniformId();
+
+	if (modelMatixId == -1) {
+		std::cout << "Error: Can't perform transformation " << std::endl;
+		return;
+	}
+	if (renderModeId == -1) {
+		std::cout << "Error: Can't set renderMode in ImageObject " << std::endl;
+		return;
+	}
+
+	squareMesh->ChangeTextureData(sheetInfo.row, sheetInfo.column, sheetInfo.spritewidth, sheetInfo.spriteheight, sheetInfo.sheetwidth, sheetInfo.sheetheight);
+
+	std::vector <glm::mat4> matrixStack;
+
+	glm::mat4 currentMatrix = this->getTransform();
+
+	if (squareMesh != nullptr) {
+
+		currentMatrix = globalModelTransform * currentMatrix;
+		glUniformMatrix4fv(modelMatixId, 1, GL_FALSE, glm::value_ptr(currentMatrix));
+		glUniform1i(renderModeId, 1);
+		glBindTexture(GL_TEXTURE_2D, texture);
+		squareMesh->Render();
+
+	}
+}
+
+void TrapObject::setLifeTime(int lifeTime) {
+	this->lifeTime = lifeTime;
+}
+
+void TrapObject::reduceLifeTime() {
+	lifeTime -= 1;
+}
+int TrapObject::getLifetime() {
+	return lifeTime;
+}
+
+void TrapObject::setNumOwner(int num) {
+	playerNumOwner = num;
+}
+
+int TrapObject::getNumOwner() {
+	return playerNumOwner;
+}
